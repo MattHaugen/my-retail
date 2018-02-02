6a5d2442a1e5d1de4775f455172ab9849af38a3a
Matt Haugen <matthew.haugen@target.com>
2018-02-01 21:50:24 -0600

    Add Styled Components

--- src/components/ProductTitle.js
+++ src/components/ProductTitle.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.h1`
+  font-size: 26px;
+  text-align: center;
+  font-weight: 200;
+  padding-left: 25px;
+  padding-right: 25px;
+`;
 
 const ProductTitle = ({ title }) => (
-  <div className="product-title">{title}</div>
+  <Header>{title}</Header>
 )
 
 export default ProductTitle;
--- src/containers/ProductDetailContainer.js
+++ src/containers/ProductDetailContainer.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import ProductTitle from '../components/ProductTitle';
 
+const StyledProductDetailContainer = styled.div`
+  display: flex;
+  width: 1000px;
+`;
+
+const StyledLeftColumn = styled.div`
+  width: 475px;
+  margin-right: 25px;
+`;
+
+const StyledRightColumn = styled.div`
+  width: 500px;
+`;
+
 const ProductDetailContainer = ({item}) => (
-  <div className="product-detail-container">
-    <ProductTitle title={item.title} />
-  </div>
+  <StyledProductDetailContainer>
+    <StyledLeftColumn>
+      <ProductTitle title={item.title} />
+    </StyledLeftColumn>
+    <StyledRightColumn>
+
+    </StyledRightColumn>
+  </StyledProductDetailContainer>
 );
 
 const mapStateToProps = state => ({
