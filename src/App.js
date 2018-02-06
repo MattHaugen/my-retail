18d7b5ebaea78cc1e8c3421ffa2ee924e5cc620c
Matt Haugen <matthew.haugen@target.com>
2018-02-05 22:18:05 -0600

    Remove unused CSS file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ProductDetailContainer from './containers/ProductDetailContainer';
-import './App.css';
 
 class App extends Component {
   render() {
